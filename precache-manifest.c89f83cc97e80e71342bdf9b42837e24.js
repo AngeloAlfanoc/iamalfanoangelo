c9a71d0b2d6ebbc8df115552dd1c9fffcf34a117
Angelo <alfanoangeloc@gmail.com>
2019-08-24 14:01:57 +0200

    new commit

---
+++ precache-manifest.c89f83cc97e80e71342bdf9b42837e24.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25f05267bc7c7960becb3b17ff30481",
+    "url": "/iamalfanoangelo/index.html"
+  },
+  {
+    "revision": "6b89d15c8809373ba897",
+    "url": "/iamalfanoangelo/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a3c168612131687f9ed0",
+    "url": "/iamalfanoangelo/static/css/main.a8450074.chunk.css"
+  },
+  {
+    "revision": "6b89d15c8809373ba897",
+    "url": "/iamalfanoangelo/static/js/2.b4efcd45.chunk.js"
+  },
+  {
+    "revision": "a3c168612131687f9ed0",
+    "url": "/iamalfanoangelo/static/js/main.a8d9917a.chunk.js"
+  },
+  {
+    "revision": "6df0e650b8b86ede0ee5",
+    "url": "/iamalfanoangelo/static/js/runtime~main.ba1ddc9b.js"
+  },
+  {
+    "revision": "d1b128f1172fba7baab9ac1c08f20dcc",
+    "url": "/iamalfanoangelo/static/media/animaldetective.d1b128f1.PNG"
+  },
+  {
+    "revision": "730100f25a8df52b26c26d2ce3364a4b",
+    "url": "/iamalfanoangelo/static/media/faceithavewemet.730100f2.PNG"
+  },
+  {
+    "revision": "b9da2244b48e97577590dd8137200f51",
+    "url": "/iamalfanoangelo/static/media/memphis-mini-dark.b9da2244.png"
+  }
+]);
